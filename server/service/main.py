e0aee03fc52c9038ffb7b82aae05d7be243ad421
dialectics <stav_ares@hotmail.com>
2022-10-01 11:35:23 +0300

    Minimized python requirements

--- server/service/main.py
+++ server/service/main.py
@@ -13,10 +13,45 @@ from obspy.core import read
 from obspy.core import UTCDateTime
 
 
+def exampleReadSTation():
+    station = "https://pds-geosciences.wustl.edu/lunar/urn-nasa-pds-apollo_pse/data/xa/metadata/stationxml.xa.0.sxml"
+    inv = obspy.read_inventory(station)
+    print(inv)
+    
+    ch = inv.select(channel="*MH1")
+    print(ch)
+
+    
+    lst = inv.get_contents()
+    print(lst)
+    
+    net = inv[0]
+    print(net)
+    
+    st = net[0]
+    print(st)
+    
+    ch = st[0]
+    print(ch)
+    
+    print(ch.source_id)
+   # ch.plot(0.001)
+    
+    #stat = inv.select(station="XA.S15*", channel="*.MH1", network="XA")
+    #print(stat, inv)
+    
+    
+    a = 'https://pds-geosciences.wustl.edu/lunar/urn-nasa-pds-apollo_pse/data/xa/continuous_waveform/s11/1969/202/xa.s11..att.1969.202.0.mseed'
+    s = read(a)
+    s.plot()
+    return
+
+
 
 def main():
     print("Next line should print 2012-09-07T12:15:00.000000Z")
     print(UTCDateTime("2012-09-07T12:15:00"))
+    #exampleReadSTation()
 
 if __name__ == "__main__":
     main()
